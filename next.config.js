ce5a17d959639d0cd61b531abdba23c70ee62045
Nikki Abarca <abarcanikki@gmail.com>
2022-08-22 23:25:01 +0800

    chore: add initial libraries and files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins")
+const withSvgr = require("next-svgr")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([withBundleAnalyzer, withSvgr], nextConfig)
